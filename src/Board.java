ec419a568349e5bdb694a7a0d2f9177e6bee9cbf
PiSauce <53952729+PiSauce@users.noreply.github.com>
2020-02-27 13:45:51 -0700

    added board

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
 
 public class Board {
-
+	ArrayList<Property> propertyList = new ArrayList<Property>();
+	Deck chanceDeck;
+	Deck chestDeck;
+	
+	public void initProperties() {
+		// Add properties to propertyList in order
+	}
+	
+	public void initChanceDeck() {
+		chanceDeck = new Deck();
+		for(int i = 0; i < 15/*The size of the deck we want*/; i++) {
+			chanceDeck.addCard(new ChanceCard(i));
+		}
+		chanceDeck.shuffle();
+	}
+	
+	public void initChestDeck() {
+		chestDeck = new Deck();
+		for(int i = 0; i < 15/*The size of the deck we want*/; i++) {
+			chestDeck.addCard(new ChanceCard(i));
+		}
+		chestDeck.shuffle();
+	}
+	
+	public Property getProperty(int pos) {
+		return propertyList.get(pos);
+	}
+	
+	public void setOwner(int pos, int turn) {
+		propertyList.get(pos).setOwner(turn);
+	}
+	
+	public Card drawChance() {
+		return chanceDeck.draw();
+	}
+	
+	public Card drawChest() {
+		return chestDeck.draw();
+	}
 }
