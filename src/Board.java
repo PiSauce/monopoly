4dd39fea52a0d8b05ffab03452eb5196199b5fab
dawoodafzaal <60233319+dawoodafzaal@users.noreply.github.com>
2020-02-28 21:21:28 -0700

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+// board class, this is the main class which deals with all the sub classes such as deck, cards, etc
+//chance and chest are the child classes of the deck class
+
 public class Board {
 	ArrayList<Property> propertyList = new ArrayList<Property>();
 	Deck chanceDeck;
@@ -10,6 +13,7 @@ public class Board {
 		// Add properties to propertyList in order 
 	}
 	
+	// creating and initializing the chance deck of cards
 	public void initChanceDeck() {
 		chanceDeck = new Deck();
 		for(int i = 0; i < 15/*The size of the deck we want*/; i++) {
@@ -18,6 +22,7 @@ public class Board {
 		chanceDeck.shuffle();
 	}
 	
+	// creating and initializing the community chest cards
 	public void initChestDeck() {
 		chestDeck = new Deck();
 		for(int i = 0; i < 15/*The size of the deck we want*/; i++) {
@@ -26,22 +31,27 @@ public class Board {
 		chestDeck.shuffle();
 	}
 	
+	// gets property a player purchases on the board
 	public Property getProperty(int pos) {
 		return propertyList.get(pos);
 	}
 	
+	// gets property and assigns a owner to the purchased property
 	public void setOwner(int pos, int turn) {
 		propertyList.get(pos).setOwner(turn);
 	}
-	
+	// draws a card from the chance deck
 	public Card drawChance() {
 		return chanceDeck.draw();
 	}
 	
+	//draw chest is invoked when a player lands on a chest on the board and is made to draw a card
 	public Card drawChest() {
 		return chestDeck.draw();
 	}
 	
+	// get action simply reads the card and places the effect into action (ex. lose $100 and the getAction is the getter 
+	// for doing the action of the card and returns the action
 	public int getAction() {
 		return action;
 	}
