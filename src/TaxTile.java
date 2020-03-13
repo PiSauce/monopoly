1217b09baf351f522dee830ced0ec04f8c06eed9
PiSauce <pisauce@hotmail.com>
2020-03-12 20:14:07 -0600

    Added getter and setter for tax

--- src/TaxTile.java
+++ src/TaxTile.java
@@ -1,7 +1,17 @@
 public class TaxTile extends Tile {
+    private int tax;
+
     public TaxTile(String name){
         super(name);
-        if(name == "Income Tax") setAction(10); // Remove $200
-        if(name == "Luxury Tax") setAction(11); // Remove $100
+        if(name == "Income Tax") setTax(200);; // Remove $200
+        if(name == "Luxury Tax") setTax(100);; // Remove $100
+    }
+
+    public int getTax() {
+        return tax;
+    }
+
+    public void setTax(int tax) {
+        this.tax = tax;
     }
 }
