ef178a22c2f4f0c099ad0d5168c1f23001ea6720
PiSauce <pisauce@hotmail.com>
2020-03-12 20:58:06 -0600

    removed actions from Tile

--- src/Tile.java
+++ src/Tile.java
@@ -1,9 +1,6 @@
-import java.util.ArrayList;
-
 public abstract class Tile {
     // Instance variables
     private String name;
-    private ArrayList<Integer> actions;
 
     public Tile(String name){
         this.name = name;
@@ -15,23 +12,5 @@ public abstract class Tile {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public ArrayList<Integer> getActions() {
-        return actions;
-    }
-
-    public void addAction(int action) {
-        this.actions.add(action);
-    }
-
-    public void removeAction(int action) {
-        for (int i = 0; i < actions.size(); i++) {
-            if(actions.get(i) == action) actions.remove(i);
-        }
-    }
-
-    public void clearActions() {
-        this.actions.clear();
     }
 }
