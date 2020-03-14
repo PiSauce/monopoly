9f3b44a3a53e06e1cd16c243a71d90b6b029b229
PiSauce <pisauce@hotmail.com>
2020-03-14 14:13:35 -0600

    added user input

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
 	private int turnNo = 0;
@@ -7,6 +8,9 @@ public class Game {
 	
 	public void newGame(int playerCount) {
 		board = new Board();
+		board.initTiles();
+		board.initChanceDeck();
+		board.initChestDeck();
 		playerList.clear();
 		for(int i = 0; i < playerCount; i++) {
 			playerList.add(new Player(i, 0));
@@ -18,13 +22,14 @@ public class Game {
 	}
 	
 	public void turn(Board board) {
+		ArrayList<Integer> validActions = new ArrayList<Integer>();
 		int counter = 0;
 
 		// Get current player
 		Player player = playerList.get(turnNo);
 		boolean doubles = false;
 
-		System.out.println("It is player " + turnNo + "'s turn.");
+		System.out.println("It is player " + (turnNo + 1) + "'s turn.");
 		
 		do {
 		// Roll dice
@@ -33,6 +38,7 @@ public class Game {
 		counter += doubles ? 1:0;
 
 		System.out.println("Rolled a " + (dice[0] + dice[1]) + "! (" + dice[0] + " and " + dice[1] + ")");
+		if(doubles) System.out.println("Doubles!");;
 
 		if(counter == 3){ // If player has rolled a double 3 times
 			System.out.println("Rolled a double 3 times! Player " + player.getTurnNum() + " has been sent to jail!");
@@ -49,8 +55,8 @@ public class Game {
 		// Make any transactions
 		if(curTile instanceof Property){ // If current tile is a property
 			Property tile = (Property) curTile;
-			ArrayList<Integer> validActions = new ArrayList<Integer>();
 
+			// Print out valid actions
 			if(tile.getOwner() == -1) { // If property is unowned
 				System.out.println("0: Buy " + tile.getName() + "? $" + tile.getPrice());
 				validActions.add(0);
@@ -72,7 +78,13 @@ public class Game {
 					}
 				}
 			}
-			
+
+			int input = checkInput(validActions);
+			doAction(input, player, tile);
+		}
+
+		if(curTile instanceof Railroad){ // If current tile is a railroad
+
 		}
 
 		if(curTile instanceof TaxTile){ // If current tile is a tax tile
@@ -93,7 +105,30 @@ public class Game {
 		} while(doubles);
 		
 		// Increment turn
-		turnNo++;
+		if(++turnNo % playerList.size() == 0) turnNo--;
+	}
+
+	private int checkInput(ArrayList<Integer> validActions) {
+		Scanner in = new Scanner(System.in);
+		boolean valid = false;
+		int input = -1;
+
+		while (!valid) {
+			System.out.println("Type an action: ");
+			String userInput = in.nextLine();
+			try {
+				input = Integer.parseInt(userInput);
+			} catch (Exception e) {
+				continue;
+			}
+			if (validActions.contains(input)) {
+				valid = true;
+			} else {
+				valid = false;
+			}
+		}
+
+		return input;
 	}
 
 	// Tax tile actions
@@ -120,8 +155,11 @@ public class Game {
 				if(property.getOwner() == -1 && player.getMoney() >= property.getPrice()){ // If unowned and player has enough money
 					player.changeMoney(-property.getPrice()); // Remove money from player
 					property.setOwner(player.getTurnNum()); // Set property owner to current player
+					System.out.println("Bought " + property.getName() + "!");
+					System.out.println("Currently have $" + player.getMoney() + ".");
+					System.out.println("");
 				} else {
-					System.out.println("Property already has an owner!");
+					System.out.println("Property already has an owner!"); // Should not happen
 				}
 				break;
 			case 1: // Mortgage property
@@ -140,6 +178,8 @@ public class Game {
 	}
 	
 	private void cardAction(int action, Player currentPlayer) {
+		// 0 - 15 => Chance cards
+		// 16 - 31 => Community chest
 		switch(action) {
 		case 0:
 			currentPlayer.setPosition(0);
