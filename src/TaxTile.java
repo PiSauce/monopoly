51b3def171601d5dd6d333e05d94136be306bcb3
dawoodafzaal <60233319+dawoodafzaal@users.noreply.github.com>
2020-03-24 16:41:08 -0600

    Update TaxTile.java

--- src/TaxTile.java
+++ src/TaxTile.java
@@ -1,11 +1,14 @@
+//textile class
 public class TaxTile extends Tile {
-    private int tax;
-
+    private int tax; //instance variables
+    
+    //constructor
     public TaxTile(String name, int tax){
         super(name);
         this.tax = tax;
     }
-
+    
+    //getter and setter
     public int getTax() {
         return tax;
     }
