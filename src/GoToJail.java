2c435fccd0a9b37930a83eb9a14ae536988105aa
dawoodafzaal <60233319+dawoodafzaal@users.noreply.github.com>
2020-03-24 16:26:58 -0600

    Update GoToJail.java

--- src/GoToJail.java
+++ src/GoToJail.java
@@ -1,3 +1,5 @@
+//the class for sending a player to jail, just calls the construtor from tile
+//child class of tile and only extends parent when a player lands on the jail index on the board
 public class GoToJail extends Tile{
     public GoToJail(){
         super("Go to Jail");
