6b826a660d3c7f23993f03a7333f965b6cc6ac71
hqiao8 <60757534+hqiao8@users.noreply.github.com>
2020-03-29 22:54:22 -0600

    remove getgrouplocations overloading

--- src/Utility.java
+++ src/Utility.java
@@ -1,59 +1,47 @@
-
-//importing the necessary java files
 import javafx.scene.paint.Color;
 import javafx.scene.image.ImageView;
 
-//utility class
 public class Utility extends Landmark {
 	
-	//instance variables
 	private final int price = 150;
 	private boolean mortgaged = false;
 	private int ownerID = -1;
 	private static int groupOwnerID = -1;
 	private final int[] groupLocations = {12, 28};
 	
-	//constructor
 	public Utility(String aDescription, Color aColor, ImageView aLandmark) {
 		super(aDescription, aColor, aLandmark);
 	}
 	
-	//getter for price
 	public int getPrice() {
 		return price;
 	}
 	
-	//sets mortgage rates
 	public void setMortgaged(boolean whetherMortgaged) {
 		mortgaged = whetherMortgaged;
 	}
 	
-	//gets mortgage 
 	public boolean getMortgaged() {
 		return mortgaged;
 	}
 	
-	//sets owner (player) of the utility
 	public void setOwnerID(int anOwnerID) {
 		ownerID = anOwnerID;
 	}
 	
-	//gets the owner (player)
 	public int getOwnerID() {
 		return ownerID;
 	}
 	
-	//sets group owner
-	public void setGroupOwnerID(int aGroupOwnerID) {
+	public void setGroupOwnerID(int aLocation, int aGroupOwnerID) {
 		groupOwnerID = aGroupOwnerID;
 	}
-	//gets group owner
+	
 	public int getGroupOwnerID() {
 		return groupOwnerID;
 	}
 	
-	//getter for group location
-	public int[] getGroupLocations() {
+	public int[] getGroupLocations(int aLocation) {
 		return groupLocations;
 	}
 }
