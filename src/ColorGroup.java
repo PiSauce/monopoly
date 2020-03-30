f44165ca1f4db6414bbe9a50ae037531d86bbc9c
hqiao8 <60757534+hqiao8@users.noreply.github.com>
2020-03-29 22:48:28 -0600

    change methods for colorgroup

--- src/ColorGroup.java
+++ src/ColorGroup.java
@@ -1,24 +1,21 @@
-//importing necessary javafx files
-
 import javafx.scene.paint.Color;
 import javafx.scene.image.ImageView;
 
-
-
 public class ColorGroup extends Landmark {
-	//instance variables
+	
 	private int price;
+	private boolean toBeImproved = true;
 	private int buildingCost;
-	private int houseNumber = 0;
-	private int hotelNumber = 0;
+	private static int availableHouseNumber = 32;
+	private static int availableHotelNumber = 12;
+	private int buildingNumber = 0;
 	private int[] rents;
+	private boolean toBeUnimproved = false;
 	private boolean mortgaged = false;
 	private int ownerID = -1;
 	private static int[] groupOwnerIDs = {-1, -1, -1, -1, -1, -1, -1, -1};
 	private final int[][] groupLocations = {{1, 3}, {6, 8, 9}, {11, 13, 14}, {16, 18, 19}, {21, 23, 24}, {26, 27, 29}, {31, 32, 34}, {37, 39}};
 	
-	
-	//constructor
 	public ColorGroup(String aDescription, Color aColor, ImageView aLandmark, int aPrice, int someBuildingCost, int[] someRents) {
 		super(aDescription, aColor, aLandmark);
 		setPrice(aPrice);
@@ -26,114 +23,110 @@ public class ColorGroup extends Landmark {
 		setRents(someRents);
 	}
 	
-	//setter for price
 	public void setPrice(int aPrice) {
 		price = aPrice;
 	}
 	
-	
-	//getter for price
 	public int getPrice() {
 		return price;
 	}
 	
+	public void setToBeImproved(boolean whetherToBeImproved) {
+		toBeImproved = whetherToBeImproved;
+	}
 	
-	//setter for building
+	public boolean getToBeImproved() {
+		return toBeImproved;
+	}
+	
 	public void setBuildingCost(int someBuildingCost) {
 		buildingCost = someBuildingCost;
 	}
 	
-	
-	//getter for building
 	public int getBuildingCost() {
 		return buildingCost;
 	}
 	
-	
-	//adding houses based by number
-	public void addHouseNumber() {
-		houseNumber++;
+	public void addAvailableHouseNumber() {
+		availableHouseNumber++;
 	}
 	
-	
-	//reduce house number
-	public void reduceHouseNumber() {
-		houseNumber--;
+	public void reduceAvailableHouseNumber() {
+		availableHouseNumber--;
 	}
 	
-	
-	//getter for house number
-	public int getHouseNumber() {
-		return houseNumber;
+	public int getAvailableHouseNumber() {
+		return availableHouseNumber;
 	}
 	
-	//adding hotel number
-	public void addHotelNumber() {
-		hotelNumber++;
+	public void addAvailableHotelNumber() {
+		availableHotelNumber++;
 	}
 	
-	
-	//reduced number of hotels
-	public void reduceHotelNumber() {
-		hotelNumber--;
+	public void reduceAvailableHotelNumber() {
+		availableHotelNumber--;
 	}
 	
-	
-	//gets hotel number
-	public int getHotelNumber() {
-		return hotelNumber;
+	public int getAvailableHotelNumber() {
+		return availableHotelNumber;
 	}
 	
+	public void addBuildingNumber() {
+		buildingNumber++;
+	}
 	
-	//sets rent prices
+	public void reduceBuildingNumber() {
+		buildingNumber--;
+	}
+	
+	public int getBuildingNumber() {
+		return buildingNumber;
+	}
+	
 	public void setRents(int[] someRents) {
 		rents = someRents;
 	}
 	
-	
-	//getter for rents
 	public int[] getRents() {
 		return rents;
 	}
 	
-	//setter for mortgage rates
+	public void setToBeUnimproved(boolean whetherToBeUnimproved) {
+		toBeUnimproved = whetherToBeUnimproved;
+	}
+	
+	public boolean getToBeUnimproved() {
+		return toBeUnimproved;
+	}
+	
 	public void setMortgaged(boolean whetherMortgaged) {
 		mortgaged = whetherMortgaged;
 	}
 	
-	//get mortgage
 	public boolean getMortgaged() {
 		return mortgaged;
 	}
 	
-	//set owner 
 	public void setOwnerID(int anOwnerID) {
 		ownerID = anOwnerID;
 	}
 	
-	
-	//getter for ID
 	public int getOwnerID() {
 		return ownerID;
 	}
 	
-	//setter for groupowner
 	public void setGroupOwnerID(int aLocation, int aGroupOwnerID) {
 		groupOwnerIDs[getGroupID(aLocation)] = aGroupOwnerID;
 	}
 	
-	//getter for group owner
 	public int getGroupOwnerID(int aLocation) {
 		return groupOwnerIDs[getGroupID(aLocation)];
 	}
 	
-	
-	//gets group location
 	public int[] getGroupLocations(int aLocation) {
 		return groupLocations[getGroupID(aLocation)];
 	}
 	
-	//getter for group ID, returns group ID
 	public int getGroupID(int aLocation) {
 		int groupID = 0;
 		loop:
