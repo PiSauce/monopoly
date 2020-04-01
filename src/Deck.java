fd0787c174686af0b6be63fe492c71d54f8c5d23
dawoodafzaal <60233319+dawoodafzaal@users.noreply.github.com>
2020-03-31 23:28:19 -0600

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 
+//deck class
 public class Deck {
-	private ArrayList<Card> cards = new ArrayList<Card>();
+	private ArrayList<Card> cards = new ArrayList<Card>(); //creating new array of cards
 	
-	public void shuffle() {
+	public void shuffle() { //shuffle method
 		for(int i = 0; i < 50; i++) {
-			int r1 = (int)Math.random()*cards.size();
+			int r1 = (int)Math.random()*cards.size(); //randomizes cards
 			int r2 = (int)Math.random()*cards.size();
 			Card temp = cards.get(r1);
 			cards.remove(r1);
@@ -15,11 +16,11 @@ public class Deck {
 		}
 	}
 	
-	public void addCard(Card c) {
+	public void addCard(Card c) { //adds cards to the deck
 		cards.add(c);
 	}
 	
-	public Card draw() {
+	public Card draw() { //returns card drawn
 		Card card = cards.get(0);
 		cards.remove(0);
 		cards.add(cards.size(), card);
