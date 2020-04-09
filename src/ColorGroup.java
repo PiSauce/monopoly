0f909ac57f2c61d6f9fe5e3996945958dbe4a1eb
dawoodafzaal <60233319+dawoodafzaal@users.noreply.github.com>
2020-04-09 14:06:58 -0600

    Update ColorGroup.java

--- src/ColorGroup.java
+++ src/ColorGroup.java
@@ -1,8 +1,11 @@
 import javafx.scene.paint.Color;
 import javafx.scene.image.ImageView;
 
+//extension of landmark, mainly deals with getting building costs and-
+//total number of available houses/hotels
 public class ColorGroup extends Landmark {
 	
+	//instance variables
 	private int price;
 	private boolean toBeImproved = true;
 	private int buildingCost;
@@ -16,6 +19,7 @@ public class ColorGroup extends Landmark {
 	private static int[] groupOwnerIDs = {-1, -1, -1, -1, -1, -1, -1, -1};
 	private final int[][] groupLocations = {{1, 3}, {6, 8, 9}, {11, 13, 14}, {16, 18, 19}, {21, 23, 24}, {26, 27, 29}, {31, 32, 34}, {37, 39}};
 	
+	//constructor
 	public ColorGroup(String aDescription, Color aColor, ImageView aLandmark, int aPrice, int someBuildingCost, int[] someRents) {
 		super(aDescription, aColor, aLandmark);
 		setPrice(aPrice);
@@ -23,6 +27,7 @@ public class ColorGroup extends Landmark {
 		setRents(someRents);
 	}
 	
+	//setter and getter for price
 	public void setPrice(int aPrice) {
 		price = aPrice;
 	}
@@ -31,6 +36,7 @@ public class ColorGroup extends Landmark {
 		return price;
 	}
 	
+	//setter and getter for improvement
 	public void setToBeImproved(boolean whetherToBeImproved) {
 		toBeImproved = whetherToBeImproved;
 	}
@@ -39,6 +45,7 @@ public class ColorGroup extends Landmark {
 		return toBeImproved;
 	}
 	
+	//setter and getter for building costs
 	public void setBuildingCost(int someBuildingCost) {
 		buildingCost = someBuildingCost;
 	}
@@ -47,34 +54,44 @@ public class ColorGroup extends Landmark {
 		return buildingCost;
 	}
 	
-	public void addAvailableHouseNumber() {
+	//adds to total number of available houses
+	//an exmaple of when this method is applicable is when a player sells a house, then the total number of available houses increases
+	public void addAvailableHouseNumber() { 
 		availableHouseNumber++;
 	}
 	
+	//reduced total number of houses available
+	//an example would be if a player purchases a house then the total number of houses decreases
 	public void reduceAvailableHouseNumber() {
 		availableHouseNumber--;
 	}
 	
+	//gets the available number of houses a player can purchase
 	public int getAvailableHouseNumber() {
 		return availableHouseNumber;
 	}
 	
+	//adds to total number of available hotel numbers
 	public void addAvailableHotelNumber() {
 		availableHotelNumber++;
 	}
 	
+	//reduces the available number of hotels
 	public void reduceAvailableHotelNumber() {
 		availableHotelNumber--;
 	}
 	
+	//gets the total number of hotels available to purchase
 	public int getAvailableHotelNumber() {
 		return availableHotelNumber;
 	}
 	
+	//add building number adds the number of building owned by a player
 	public void addBuildingNumber() {
 		buildingNumber++;
 	}
 	
+	//reduce building number reduces the number of building owned by a player
 	public void reduceBuildingNumber() {
 		buildingNumber--;
 	}
@@ -83,6 +100,7 @@ public class ColorGroup extends Landmark {
 		return buildingNumber;
 	}
 	
+	//setter and getter for rent
 	public void setRents(int[] someRents) {
 		rents = someRents;
 	}
@@ -99,14 +117,17 @@ public class ColorGroup extends Landmark {
 		return toBeUnimproved;
 	}
 	
+	//checks if a player has a property on mortgage
 	public void setMortgaged(boolean whetherMortgaged) {
 		mortgaged = whetherMortgaged;
 	}
 	
+	//bool; so if true that means the player mortgaged their porpoerty and false otherwise
 	public boolean getMortgaged() {
 		return mortgaged;
 	}
 	
+	//setter and getter for retrieving owner ID
 	public void setOwnerID(int anOwnerID) {
 		ownerID = anOwnerID;
 	}
@@ -115,10 +136,12 @@ public class ColorGroup extends Landmark {
 		return ownerID;
 	}
 	
+	//group ID referes to a group of property owned by a single player
 	public void setGroupOwnerID(int aLocation, int aGroupOwnerID) {
 		groupOwnerIDs[getGroupID(aLocation)] = aGroupOwnerID;
 	}
 	
+	//setter and getter for group owner
 	public int getGroupOwnerID(int aLocation) {
 		return groupOwnerIDs[getGroupID(aLocation)];
 	}
@@ -127,6 +150,9 @@ public class ColorGroup extends Landmark {
 		return groupLocations[getGroupID(aLocation)];
 	}
 	
+	//Name: getGroupID
+	//Parameters (int aLocation)
+	//What it Does: retrieves a group ID when taking an instance of their location, loops through various locations and if 
 	public int getGroupID(int aLocation) {
 		int groupID = 0;
 		loop:
