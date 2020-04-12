61da7341b1a71da86626d126afc2340affaa1a09
hqiao8 <60757534+hqiao8@users.noreply.github.com>
2020-04-12 15:19:49 -0600

    Last update of TaxSpace class

--- src/TaxSpace.java
+++ src/TaxSpace.java
@@ -1,23 +1,18 @@
-
-//importing java fx
 import javafx.scene.paint.Color;
-import javafx.scene.image.ImageView;
 
-//tax space class
 public class TaxSpace extends Landmark {
-	//instance variable
+	
 	private int penalties;
 	
-	//constructor
-	public TaxSpace(String aDescription, Color aColor, ImageView aLandmark, int somePenalties) {
-		super(aDescription, aColor, aLandmark);
+	public TaxSpace(String aDescription, Color aColor, int somePenalties) {
+		super(aDescription, aColor);
 		setPenalties(somePenalties);
 	}
-	//setter for penalities
+	
 	public void setPenalties(int somePenalties) {
 		penalties = somePenalties;
 	}
-	//gets the penalty
+	
 	public int getPenalties() {
 		return penalties;
 	}
